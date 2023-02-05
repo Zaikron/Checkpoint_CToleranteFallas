f8419ea71b75810c1ae4743e38613212d613dfb9
Anthony Esteven Sandoval Marquez <anthony.sandoval1805@gmail.com>
2023-02-05 11:50:47 -0600

    Add files via upload

---
+++ Checkpoint/Checkpoint/Principal.Designer.cs
@@ -0,0 +1,195 @@
+﻿
+namespace Checkpoint
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.drawTimer = new System.Windows.Forms.Timer(this.components);
+            this.drawArea = new System.Windows.Forms.PictureBox();
+            this.tamBrush = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnColor = new System.Windows.Forms.Button();
+            this.colorDialog = new System.Windows.Forms.ColorDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirProyectoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkBoxSave = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.drawArea)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // drawTimer
+            // 
+            this.drawTimer.Interval = 60000;
+            this.drawTimer.Tick += new System.EventHandler(this.drawTimer_Tick);
+            // 
+            // drawArea
+            // 
+            this.drawArea.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.drawArea.Location = new System.Drawing.Point(93, 27);
+            this.drawArea.Name = "drawArea";
+            this.drawArea.Size = new System.Drawing.Size(546, 300);
+            this.drawArea.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.drawArea.TabIndex = 0;
+            this.drawArea.TabStop = false;
+            this.drawArea.MouseDown += new System.Windows.Forms.MouseEventHandler(this.drawArea_MouseDown);
+            this.drawArea.MouseMove += new System.Windows.Forms.MouseEventHandler(this.drawArea_MouseMove);
+            this.drawArea.MouseUp += new System.Windows.Forms.MouseEventHandler(this.drawArea_MouseUp);
+            // 
+            // tamBrush
+            // 
+            this.tamBrush.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.tamBrush.FormattingEnabled = true;
+            this.tamBrush.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "4",
+            "6",
+            "8",
+            "10",
+            "16",
+            "24",
+            "36",
+            "48",
+            "62",
+            "74",
+            "86",
+            "98",
+            "112"});
+            this.tamBrush.Location = new System.Drawing.Point(8, 49);
+            this.tamBrush.Name = "tamBrush";
+            this.tamBrush.Size = new System.Drawing.Size(79, 21);
+            this.tamBrush.TabIndex = 1;
+            this.tamBrush.Tag = "";
+            this.tamBrush.SelectedIndexChanged += new System.EventHandler(this.tamBrush_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tamaño";
+            // 
+            // btnColor
+            // 
+            this.btnColor.Location = new System.Drawing.Point(8, 105);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(79, 23);
+            this.btnColor.TabIndex = 4;
+            this.btnColor.Text = "Color";
+            this.btnColor.UseVisualStyleBackColor = true;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(651, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.guardarToolStripMenuItem,
+            this.abrirProyectoToolStripMenuItem});
+            this.toolStripMenuItem1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripMenuItem1.ForeColor = System.Drawing.SystemColors.Control;
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(62, 20);
+            this.toolStripMenuItem1.Text = "Archivo";
+            // 
+            // guardarToolStripMenuItem
+            // 
+            this.guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
+            this.guardarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.guardarToolStripMenuItem.Text = "Guardar";
+            this.guardarToolStripMenuItem.Click += new System.EventHandler(this.guardarToolStripMenuItem_Click);
+            // 
+            // abrirProyectoToolStripMenuItem
+            // 
+            this.abrirProyectoToolStripMenuItem.Name = "abrirProyectoToolStripMenuItem";
+            this.abrirProyectoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.abrirProyectoToolStripMenuItem.Text = "Abrir Proyecto";
+            this.abrirProyectoToolStripMenuItem.Click += new System.EventHandler(this.abrirProyectoToolStripMenuItem_Click);
+            // 
+            // checkBoxSave
+            // 
+            this.checkBoxSave.AutoSize = true;
+            this.checkBoxSave.Location = new System.Drawing.Point(11, 310);
+            this.checkBoxSave.Name = "checkBoxSave";
+            this.checkBoxSave.Size = new System.Drawing.Size(76, 17);
+            this.checkBoxSave.TabIndex = 6;
+            this.checkBoxSave.Text = "Auto Save";
+            this.checkBoxSave.UseVisualStyleBackColor = true;
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 339);
+            this.Controls.Add(this.checkBoxSave);
+            this.Controls.Add(this.btnColor);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tamBrush);
+            this.Controls.Add(this.drawArea);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Principal";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Principal_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.drawArea)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer drawTimer;
+        private System.Windows.Forms.PictureBox drawArea;
+        private System.Windows.Forms.ComboBox tamBrush;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnColor;
+        private System.Windows.Forms.ColorDialog colorDialog;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem guardarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirProyectoToolStripMenuItem;
+        private System.Windows.Forms.CheckBox checkBoxSave;
+    }
+}
+
---
+++ Checkpoint/Checkpoint/Principal.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Checkpoint
+{
+    public partial class Principal : Form
+    {
+        private int x;
+        private int y;
+        bool drawMode = false;
+
+        Graphics g;
+        Brush brush;
+        Bitmap bmp;
+        int brushRadio = 6;
+        Color brushColor = Color.Black;
+
+        public Principal()
+        {
+            InitializeComponent();
+
+            bmp = new Bitmap(drawArea.Width, drawArea.Height);
+            g = Graphics.FromImage(bmp);
+            g.Clear(Color.White);
+            drawArea.Image = bmp;
+
+            brush = new SolidBrush(brushColor);
+
+            drawTimer.Start();
+        }
+
+        //Dibuja un circulo
+        void drawCircle(int x, int y, int r)
+        {
+            g.FillEllipse(brush, (x - r), (y - r), (2 * r), (2 * r));
+            drawArea.Image = bmp;
+        }
+
+        //Eventos: Movimiento, mouse presionado, mouse soltado
+        private void drawArea_MouseMove(object sender, MouseEventArgs e)
+        {
+            x = e.X;
+            y = e.Y;
+
+            if (drawMode == true)
+            {
+                drawCircle(x, y, brushRadio);
+            }
+        }
+
+        private void drawArea_MouseDown(object sender, MouseEventArgs e)
+        {
+            drawMode = true;
+        }
+
+        private void drawArea_MouseUp(object sender, MouseEventArgs e)
+        {
+            drawMode = false;
+        }
+
+        //Comboboxes: tamaño de brocha, colores
+        private void tamBrush_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            brushRadio = int.Parse(tamBrush.SelectedIndex.ToString());
+        }
+
+        //Timer: Guardado cada minuto
+        private void drawTimer_Tick(object sender, EventArgs e)
+        {
+            if (checkBoxSave.Checked == true)
+            {
+                Debug.WriteLine("saving...");
+                createCheckpoint();
+            }
+        }
+
+        //Seleccionar el color de pintado
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                brushColor = colorDialog.Color;
+                brush = new SolidBrush(brushColor);
+            }
+        }
+
+        //Guardar la imagen
+        private void guardarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "JPEG(*.JPG)|*.JPG|BMP(*.BMP)|*.BMP|PNG(*.PNG)|*.PNG";
+
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                createCheckpoint();
+                bmp.Save(save.FileName);
+            }
+        }
+
+        //Guardado de seguridad del archivo
+        private void createCheckpoint()
+        {
+            String date = DateTime.Now.ToString("yyyy_MM_dd_THH_mm_ss", System.Globalization.CultureInfo.InvariantCulture);
+            Debug.WriteLine(date);
+            try
+            {
+                StreamWriter sw = new StreamWriter(".\\" + date + ".smae");
+                for (y = 0; y < bmp.Height; y++)
+                {
+                    for (x = 0; x < bmp.Width; x++)
+                    {
+                        //Formato: x|y|(color->) r|g|b
+                        sw.WriteLine(x + "|" + y + "|" + 
+                                    bmp.GetPixel(x, y).R + "|" +
+                                    bmp.GetPixel(x, y).G + "|" +
+                                    bmp.GetPixel(x, y).B + "|");
+                    }
+                }
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+
+        //Funcion para abrir y restaurar un archivo
+        private void restoreProject()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Filter = "SMAE(*.SMAE)|*.SMAE";
+            openFileDialog.InitialDirectory = ".\\..";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                String filePath = openFileDialog.FileName;
+                String line;
+                try
+                {
+                    StreamReader sr = new StreamReader(filePath);
+                    line = sr.ReadLine();
+                    while (line != null)
+                    {
+                        //Formato: x|y|(color->) r|g|b
+                        //         0 1           2 3 4
+                        List<String> values = getValues(line);
+                        Color currentColor = Color.FromArgb(int.Parse(values[2]), int.Parse(values[3]), int.Parse(values[4]));
+                        bmp.SetPixel(int.Parse(values[0]), int.Parse(values[1]), currentColor);
+                        line = sr.ReadLine();
+                    }
+                    sr.Close();
+                    Console.ReadLine();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception: " + e.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("Executing finally block.");
+                }
+                drawArea.Image = bmp;
+            }
+        }
+
+        //Obtienen los valores separados por " | "
+        private List<String> getValues(String line)
+        {
+            List<String> values = new List<String>();
+            String currentValue = "";
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (!line[i].Equals('|'))
+                {
+                    currentValue += line[i];
+                }
+                else if (line[i].Equals('|'))
+                {
+                    values.Add(currentValue);
+                    currentValue = "";
+                }
+            }
+
+            return values;
+        }
+
+        private void abrirProyectoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            restoreProject();
+        }
+
+        //Guardado de seguridad al cerrar la ventana
+        private void Principal_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (checkBoxSave.Checked == true)
+            {
+                Debug.WriteLine("saving...");
+                createCheckpoint();
+            }
+        }
+    }
+}
